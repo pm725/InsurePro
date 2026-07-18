afa1d3606da74da286a9a80cb75b1bd731641504
pm725 <mahatpriyanshu7@gmail.com>
2026-07-18 09:29:32 +0545

    Update

---
+++ src/models/User.js
@@ -0,0 +1,71 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const bcrypt = require('bcrypt');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: [3, 50]
+        }
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+            min: 18,
+            max: 100
+        }
+    },
+    bmi: {
+        type: DataTypes.DECIMAL(5, 2),
+        allowNull: true
+    },
+    smoking: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, {
+    tableName: 'users',
+    timestamps: true,
+    hooks: {
+        beforeCreate: async (user) => {
+            if (user.password) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        },
+        beforeUpdate: async (user) => {
+            if (user.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
+    }
+});
+
+// Instance method to validate password
+User.prototype.validatePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
